8a53c2373519902625cadf8fea282b5c7fce8640
Daniel Farrell <dfarrell@redhat.com>
2026-08-23 15:38:09 -0400

    k8s-rebase: remove remove-unnecessary-guard from scripts/k8s-rebase.sh
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- plugins/k8s-rebase/scripts/k8s-rebase.sh
+++ plugins/k8s-rebase/scripts/k8s-rebase.sh
@@ -920,7 +920,6 @@ if [[ -n "$NEW_GO_VERSION" ]] && [[ "$OLD_GO_VERSION" != "$NEW_GO_VERSION" ]]; t
   while IFS= read -r _df; do
     [[ -z "$_df" ]] && continue
     while IFS= read -r cur_ver; do
-      [[ -z "$cur_ver" ]] && continue
       if [[ "$cur_ver" != "$NEW_GO_SHORT" ]] && _ver_lt "$cur_ver" "$NEW_GO_SHORT"; then
         perl -pi -e "s|golang:${cur_ver//./\\.}([^0-9])|golang:${NEW_GO_SHORT}\$1|g; s|golang:${cur_ver//./\\.}\$|golang:${NEW_GO_SHORT}|g" "$_df"
         CHANGED_FILES+="$_df"$'\n'
